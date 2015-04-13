4822ff958a8712f3acd435b4a18e44d7c7d524ef
Francois van der Hoven <francois777@optusnet.com.au>
2015-04-13 16:18:08 +1000

    Fix create_admin_service (3)

--- app/services/create_admin_service.rb
+++ app/services/create_admin_service.rb
@@ -7,27 +7,31 @@ class CreateAdminService
         user.last_name = 'Administrator'
         user.admin = true
         user.role = 2
-        user.confirm!)
+        user.confirm!
+    end    
     approver = User.find_or_create_by!(email: Rails.application.secrets.approver_email) do |user|
         user.password = Rails.application.secrets.admin_password
         user.password_confirmation = Rails.application.secrets.admin_password
         user.first_name = 'Anthony'
         user.last_name = 'Approver'
         user.role = 1
-        user.confirm!)
+        user.confirm!
+    end
     owner = User.find_or_create_by!(email: Rails.application.secrets.owner_email) do |user|
         user.password = Rails.application.secrets.admin_password
         user.password_confirmation = Rails.application.secrets.admin_password
         user.first_name = 'Olivia'
         user.last_name = 'Owner'
         user.role = 2
-        user.confirm!)
+        user.confirm!
+    end    
     user = User.find_or_create_by!(email: Rails.application.secrets.user_email) do |user|
         user.password = Rails.application.secrets.admin_password
         user.password_confirmation = Rails.application.secrets.admin_password
         user.first_name = 'Ursula'
         user.last_name = 'User'
         user.role = 0
-        user.confirm!)
+        user.confirm!
+    end    
   end
 end
