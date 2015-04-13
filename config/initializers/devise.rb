7bb7f633d002e1aae32d4d80fff7983a4e087f62
Francois van der Hoven <francois777@optusnet.com.au>
2015-04-13 15:04:24 +1000

    Fixed devise settings

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,9 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'no-reply@' + Rails.application.secrets.domain_name
+  domain = Rails.application.secrets.domain_name
+  domain ||= 'example.com'
+  config.mailer_sender = 'no-reply@' + domain
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
