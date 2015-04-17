12bf95de92739a9e70eb80a373654d09c801791d
Francois van der Hoven <francois777@optusnet.com.au>
2015-04-17 19:45:37 +1000

    Add template for list artists and show artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,9 +2,16 @@ class ArtistsController < ApplicationController
 
   before_filter :authenticate_user!, only: [:create, :edit, :update, :submit]
   before_action :set_artist, except: [:new, :create, :index]
+  before_action :initialise_default_artist
 
   def show
     @context = "Artists"
+    @submitted_by = @artist.submitted_by.name
+  end
+
+  def index
+    @context = "Artists"
+    @artists = Artist.all
   end
 
   def new
@@ -20,19 +27,40 @@ class ArtistsController < ApplicationController
 
   def create
     puts "Params: #{params}"
-      # Parameters: {"utf8"=>"✓", "authenticity_token"=>"ELiv...mzbvJ==", 
-      # "artist"=>{"artist_names_attributes"=>{"0"=>{"name"=>"Aimee", "name_type"=>"0"}, 
-      #                                        "1"=>{"name"=>"Maddison", "name_type"=>"2"}}, 
-      # "gender"=>"female", "born_on"=>"29 Apr 1984", "died_on"=>"", "born_country_code"=>"cz"}, 
-      # "commit"=>"Create new artist"} 
-    redirect_to new_artist_path  
+    @artist = Artist.new(artist_params_formatted)
+    @artist.submitted_by = current_user
+    if @artist.save
+      flash[:notice] = t(:artist_submitted, scope: [:success])
+      redirect_to @artist
+    else
+      flash[:alert] = t(:artist_submit_failed, scope: [:failure])
+      set_default_categories
+      render :new
+    end
   end
 
   private
 
+    def initialise_default_artist
+    end
+
+    def artist_params_formatted
+      new_params = artist_params
+      new_params['artist_names_attributes']['0']['name_type'] = artist_params['artist_names_attributes']['0']['name_type'].to_i
+      new_params['artist_names_attributes']['1']['name_type'] = artist_params['artist_names_attributes']['1']['name_type'].to_i
+      new_params
+    end
+
     def artist_params
       params.require(:artist).
-       permit(:born_on, :died_on, :born_country_code, :historical_period_id, :gender, artist_names_attributes: [:name, :name_type])
+        permit(:born_on, :died_on, :born_country_code, :historical_period_id, :gender, artist_names_attributes: [:name, :name_type])
+    end
+
+    def set_artist
+      @artist = Artist.find(params[:id].to_i)
+    rescue
+      flash[:alert] = t(:artist_not_found, scope: [:failure]) 
+      redirect_to artists_path
     end
 
 end
